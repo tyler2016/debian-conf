4d4c495e6a67890a7ead0e559152ec055e700264
Dax <me@daxg.org>
2016-06-23 23:58:08 +0800

    optimize code

--- bin/autoxelatex.sh
+++ bin/autoxelatex.sh
@@ -6,10 +6,9 @@ old_time_stamp=''
 while true; do
     new_time_stamp=`stat ${file} | tail -3 | head -1 | cut -d ' ' -f 2`
     if [ "x"${new_time_stamp} = "x"${old_time_stamp} ]; then
-        echo `date`' wait for your edit and save.'
-        sleep 3
         continue
     fi
-    xelatex $file
     old_time_stamp=$new_time_stamp
+    xelatex $file
+    echo `date`' wait for your edit and save.'
 done
